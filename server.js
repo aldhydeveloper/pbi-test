d1e52fc1fa1fc2b72e4c3450fdf6e2342a80bf52
aldhydeveloper <67993612+aldhydeveloper@users.noreply.github.com>
2024-10-21 10:49:51 +0700

    update

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/pbi-embeded-collexe'));
 app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'dist', 'pbi-embeded-collexe', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '/dist/pbi-embeded-collexe/browser/index.html'));
 });
 app.listen(process.env.PORT || 8080);
