e738241ea485dbf38e2e5f8bc89e5ea4e58ae24c
aldhydeveloper <67993612+aldhydeveloper@users.noreply.github.com>
2024-10-21 11:05:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/pbi-embeded-collexe'));
-app.get('/*', function(req,res){res.sendFile(path.join(__dirname+'/dist/pbi-embeded-collexe/browser/index.html'));});
-app.listen(process.env.PORT || 8080);
+
+// Serve static files from the Angular dist directory
+app.use(express.static(path.join(__dirname, 'dist/pbi-embeded-collexe')));
+
+// Only serve the index.html file for non-static routes
+app.get('*', (req, res) => {
+  if (req.accepts('html')) {
+    res.sendFile(path.resolve(__dirname, 'dist/pbi-embeded-collexe', 'index.html'));
+  }
+});
+
+// Start the server on Heroku's environment port or 8080
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server is running...');
+});
